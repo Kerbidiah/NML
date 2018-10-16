803447a612d21452525a9964340872d5f1804d03
MiningXV <alexjanninck@gmail.com>
2018-10-16 16:55:11 -0400

    completly changed the different abreviations

--- NML.rb
+++ NML.rb
@@ -1,16 +1,12 @@
 #!/usr/bin/env ruby
 
 class EMType
-	#									inclusive bounds
-	def initialize(name, fullAbrv = "", abrvFormal, lowerNM, upperNM)
+	#												inclusive bounds
+	def initialize(name, fullAbrv, abrvFormal = "", lowerNM, upperNM)
 		@name = name
-		if fullAbrv == ""
-			@fullAbrv = fullAbrv
-		elsif fullAbrv != ""
-			@fullAbrv = fullAbrv
-		end
-		@abrvFormal = abrvFormal
-		@abrvInformal = abrvFormal
+		@fullAbrv = fullAbrv
+		@abrvFormal = fullAbrv
+		@abrvFormal = abrvFormal unless abrvFormal == ""
 		@lo = lowerNM.to_f
 		@hi = upperNM.to_f
 	end
@@ -25,19 +21,26 @@ class EMType
 		@abrvInformal = abrvInformal
 	end
 end
+def includes(nm)
+	i = 0
+	while i <= (self.length - 1)
+		put self[i].inspect if self[i].cover(nm)
+		i += 1
+	end
+end
 
 spectrum = Array.new
-spectrum.append EMType.new("gamma", "γ", 0.001, 0.01)
-spectrum.append EMType.new("xray", "HX", "X", 0.01, 0.2)
-spectrum.append EMType.new("xray", "SX", "X", 0.2, 10)
-spectrum.append EMType.new("ultraviolet", "EUV", "UV", 10, 124)
-spectrum.append EMType.new("ultraviolet", "NUV", "UV", 124, 400)
+spectrum.append EMType.new("gamma", "Y", "γ", 0.001, 0.01)
+spectrum.append EMType.new("xray", "HX", 0.01, 0.2)
+spectrum.append EMType.new("xray", "SX", 0.2, 10)
+spectrum.append EMType.new("ultraviolet", "EUV", 10, 124)
+spectrum.append EMType.new("ultraviolet", "NUV", 124, 400)
 spectrum.append EMType.new("visible", "V", 400, 700)
-spectrum.append EMType.new("infared", "NIR", "IR", 700, 1400)
-spectrum.append EMType.new("infared", "SWIR", "IR", 1400, 3000)
-spectrum.append EMType.new("infared", "MIR", "IR", 3000, 8000)
-spectrum.append EMType.new("infared", "LWIR", "IR", 8000, 15000)
-spectrum.append EMType.new("infared", "FIR", "IR", 15000, 10**6)
+spectrum.append EMType.new("infared", "NIR", 700, 1400)
+spectrum.append EMType.new("infared", "SWIR", 1400, 3000)
+spectrum.append EMType.new("infared", "MIR", 3000, 8000)
+spectrum.append EMType.new("infared", "LWIR", 8000, 15000)
+spectrum.append EMType.new("infared", "FIR", 15000, 10**6)
 spectrum.append EMType.new("microwaves", "EHF", 10**6, 10**7)
 spectrum.append EMType.new("microwaves", "SHF", 10**7, 10**8)
 spectrum.append EMType.new("microwaves", "UHF", 10**8, 10**9)
@@ -50,7 +53,11 @@ spectrum.append EMType.new("radio", "ULF", 10**14, 10**15)
 spectrum.append EMType.new("radio", "SLF", 10**15, 10**16)
 spectrum.append EMType.new("radio", "ELF", 10**16, 10**17)
 i = 0
-while i <= spectrum.length
-	puts spectrum[i]
+while i <= (spectrum.length - 1)
+	puts spectrum[i].inspect
 	i += 1
+end
+
+loop do
+	spectrum.include(gets.to_f)
 end
